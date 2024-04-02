9cb2e92de23fc1624183cab21ec2b4a8623f3c84
kriti.s.devatha <kriti.s.devatha@gmail.com>
2024-04-02 15:36:58 +0530

    user input to break loop

--- calc.py
+++ calc.py
@@ -1,26 +1,27 @@
 #calculator
 
 from allOperators import *
+print("press enter to stop the program")
 
+while True:
+    a = int(input("enter a:"))
+    b = int(input("enter b:"))
+    char = input("enter operator:")
 
-a = int(input("enter a:"))
-b = int(input("enter b:"))
-char = input("enter operator:")
+    if char == "+":
+        print(add2(a,b))
 
-if char == "+":
-    print(add2(a,b))
+    elif char == "-":
+        print(sub2(a,b))
 
-elif char == "-":
-    print(sub2(a,b))
+    elif char == "*":
+        print(mul2(a,b))
 
-elif char == "*":
-    print(mul2(a,b))
+    elif char == "/":
+        print(div2(a,b))
 
-elif char == "/":
-    print(div2(a,b))
+    elif char == "%":
+        print(mod2(a,b))
 
-elif char == "%":
-    print(mod2(a,b))
-
-else:
-    print("invalid you fool")
+    else:
+        print("invalid you fool")
