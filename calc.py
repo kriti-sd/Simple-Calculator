8984d43940d1a2c6911b64812601b7d96a04826c
kriti.s.devatha <kriti.s.devatha@gmail.com>
2024-04-02 10:56:38 +0530

    1st commit

---
+++ calc.py
@@ -0,0 +1,32 @@
+#calculator
+
+def add2(a,b):
+    return a + b
+
+def sub2(a,b):
+    return a - b
+
+def mul2(a,b):
+    return a * b
+
+def div2(a,b):
+    return a / b
+
+a = int(input("enter a:"))
+b = int(input("enter b:"))
+char = input("enter operator:")
+
+if char == "+":
+    print(add2(a,b))
+
+elif char == "-":
+    print(sub2(a,b))
+
+elif char == "*":
+    print(mul2(a,b))
+
+elif char == "/":
+    print(div2(a,b))
+
+else:
+    print("invalid you fool")
